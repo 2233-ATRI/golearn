657b4db054f2ce2641c7b1c4e9759b29aaaa5256
wqh <1914792977@qq.com>
2025-01-03 19:46:23 +0800

    day2

--- hello/learn2.go
+++ hello/learn2.go
@@ -2,7 +2,27 @@ package main
 
 import "fmt"
 
+const (
+	//iota
+	beijing = 10 * iota
+	xian
+	baoji
+)
+
+func lengh(string2 string) int {
+
+	fmt.Println("lengh", string2)
+	return lengh(string2)
+}
+func foo(a string, b int) (int, int) {
+	fmt.Println(b)
+	var c int = lengh(a)
+	fmt.Println(c)
+	return c, b
+}
+
 func main() {
+	const length int = 10
 	var age int16 = 10
 	fmt.Println(age)
 
---
+++ hello/learn5/lib/lib.go
@@ -0,0 +1,15 @@
+package lib
+
+import "fmt"
+
+func Libtest() {
+	fmt.Println("hello world")
+}
+
+func init() {
+	fmt.Println("hello world")
+}
+
+func main() {
+
+}
