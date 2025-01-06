26a63e4a95447eb055d2de7785191562f0bc374b
wqh <1914792977@qq.com>
2025-01-06 11:35:02 +0800

    day5

---
+++ hello/learn18.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func myfunc(arg interface{}) {
+	fmt.Println(arg)
+
+	value, ok := arg.(string)
+	if !ok {
+		fmt.Println("error!")
+	} else {
+		fmt.Println(value)
+	}
+}
+
+type Book struct {
+	auth string
+}
+
+func main() {
+	book := Book{"go"}
+	myfunc(book.auth)
+}
