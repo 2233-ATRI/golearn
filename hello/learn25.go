375f40b16c1bd12a164fc9d7f33cccb38ec56d6f
wqh <1914792977@qq.com>
2025-01-06 22:44:51 +0800

    day5

---
+++ hello/learn25.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Movie struct {
+	Title string `json:"title"`
+	Year  int    `json:"year"`
+	Price int    `json:"rmb"`
+	Actor string `json:"actor"`
+}
+
+func main() {
+	movie := Movie{"aca", 1991, 134, "wqh"}
+	fmt.Println(movie)
+	jsonstr, err := json.Marshal(movie)
+	if err != nil {
+		fmt.Println("error is ", err)
+	} else {
+		fmt.Println(string(jsonstr))
+		//fmt.Printf("%s\n", jsonstr)//等价
+	}
+}
