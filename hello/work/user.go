fe39a6c176e764e03c4e724720db8bf7f8f060aa
wqh <1914792977@qq.com>
2025-01-10 20:54:13 +0800

    tcp/ip day1

--- hello/work/user.go
+++ hello/work/user.go
@@ -3,20 +3,22 @@ package main
 import "net"
 
 type User struct {
-	Name string
-	Age  string
-	C    chan string
-	conn net.Conn
+	Name   string
+	Age    string
+	C      chan string
+	conn   net.Conn
+	Server *Server
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server Server) *User {
 
 	useraddr := conn.RemoteAddr().String()
 	user := &User{
-		Name: useraddr,
-		conn: conn,
-		C:    make(chan string),
-		Age:  useraddr,
+		Name:   useraddr,
+		conn:   conn,
+		C:      make(chan string),
+		Age:    useraddr,
+		Server: &server,
 	}
 	go user.lensten()
 	return user
@@ -27,4 +29,22 @@ func (u *User) lensten() {
 		mag := <-u.C
 		u.conn.Write([]byte(mag + "\n"))
 	}
+}
+
+func (this *User) Online() {
+	this.Server.maplock.Lock()
+	this.Server.Onlinemap[this.Name] = this
+	this.Server.maplock.Unlock()
+	this.Server.Btoadcast(this, "shangxain")
+}
+
+func (this *User) Offline() {
+	this.Server.maplock.Lock()
+	delete(this.Server.Onlinemap, this.Name)
+	this.Server.maplock.Unlock()
+	this.Server.Btoadcast(this, "xiaxian")
+}
+
+func (this *User) Domassage() {
+	this.Server.Btoadcast(User, "msg")
 }
