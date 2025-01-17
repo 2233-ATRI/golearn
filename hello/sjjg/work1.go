4a9a421b0e7811272943aeb394e4be42f60d304f
wqh <1914792977@qq.com>
2025-01-17 13:39:44 +0800

    easysaunfa

--- hello/sjjg/work1.go
+++ hello/sjjg/work1.go
@@ -4,14 +4,42 @@ import "fmt"
 
 //稀疏数组
 
+type valuenod struct {
+	long   int
+	height int
+	value  int
+}
+
 func main() {
 	var chassmap [11][11]int
 	chassmap[1][2] = 1
 	chassmap[2][3] = 2
-	for _, v := range chassmap {
-		for _, w := range v {
-			fmt.Printf("%d", w)
+	//for _, v := range chassmap {
+	//	for _, w := range v {
+	//		fmt.Printf("%d", w)
+	//	}
+	//	fmt.Println()
+	//}
+
+	var spr []valuenod
+	//valuenod := valuenod{
+	//	long:   11,
+	//	height: 11,
+	//	value:  0,
+	//}
+	for i, v := range chassmap {
+		for j, w := range v {
+			if w != 0 {
+				valuenod := valuenod{
+					long:   i,
+					height: j,
+					value:  w,
+				}
+				spr = append(spr, valuenod)
+			}
 		}
-		fmt.Println()
+	}
+	for i, v := range spr {
+		fmt.Printf("%d\t%d\t%d\t%d\n", i, v.long, v.height, v.value)
 	}
 }
