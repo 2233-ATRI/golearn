4d308ce34a8047748fd0967669ce37333f153d29
wqh <1914792977@qq.com>
2025-02-28 19:23:40 +0800

    1

--- hello/sjjg/lb.go
+++ hello/sjjg/lb.go
@@ -36,6 +36,67 @@ func (r *Ring) init() *Ring {
 	return r
 }
 
+//func newRing(n int) *Ring {
+//	if n < 1 {
+//		return nil
+//	}
+//	r := new(Ring)
+//	r.init()
+//	p := r
+//	for i := 0; i < n; i++ {
+//		p.next = &Ring{nil, nil, i}
+//		p = p.next
+//		p.prev = r
+//		r = r.next
+//	}
+//	return r
+//}
+
+//func (r *Ring) Next() *Ring {
+//	if r.next == nil {
+//		return r.init()
+//	}
+//	r = r.next
+//	return r
+//}
+//
+//func (r *Ring) Prev() *Ring {
+//	if r.prev == nil {
+//		return r.init()
+//	}
+//	r = r.prev
+//	return r
+//}
+
+//func (r *Ring) remove(n int) *Ring {
+//	if n < 0 {
+//		return nil
+//	}
+//	for ; n > 0; n-- {
+//		r.next = r.next.next
+//		r.next.prev = r.prev
+//
+//	}
+//	return r
+//}
+
+func (r *Ring) Long() int {
+	if r == nil {
+		return 0
+	}
+
+	count := 1
+	current := r.next
+
+	// 从下一个节点开始遍历，直到回到起始节点 r
+	for current != r {
+		count++
+		current = current.next
+	}
+
+	return count
+}
+
 func main() {
 	r := new(Ring)
 	r.init()
