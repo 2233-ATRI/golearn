fb11dd272d18add532ee138d46fc7a8127209bff
wqh <1914792977@qq.com>
2025-02-28 13:52:10 +0800

    1

--- hello/sjjg/lb.go
+++ hello/sjjg/lb.go
@@ -1,25 +1,43 @@
-//package main
+// package main
 //
-//import "fmt"
+// import "fmt"
 //
-//type linknode struct {
-//	next *linknode
-//	val  int
-//}
+//	type linknode struct {
+//		next *linknode
+//		val  int
+//	}
 //
-//func main() {
-//	node := new(linknode)
-//	node.val = 1
+//	func main() {
+//		node := new(linknode)
+//		node.val = 1
 //
-//	node1 := new(linknode)
-//	node1.val = 2
-//	node.next = node1
-//	for {
-//		if node == nil {
-//			return
-//		} else {
-//			fmt.Println(node.val)
-//			node = node.next
+//		node1 := new(linknode)
+//		node1.val = 2
+//		node.next = node1
+//		for {
+//			if node == nil {
+//				return
+//			} else {
+//				fmt.Println(node.val)
+//				node = node.next
+//			}
 //		}
 //	}
-//}
+package main
+
+type Ring struct {
+	next, prev *Ring
+	value      interface{}
+}
+
+func (r *Ring) init() *Ring {
+	r.next = r
+	r.prev = r
+	return r
+}
+
+func main() {
+	r := new(Ring)
+	r.init()
+
+}
