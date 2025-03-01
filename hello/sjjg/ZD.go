b8a47671e0a87bd9deb97dc8ee8d0a6d4f3e1153
wqh <1914792977@qq.com>
2025-03-01 23:08:15 +0800

    1

--- hello/sjjg/ZD.go
+++ hello/sjjg/ZD.go
@@ -1,47 +1,88 @@
-//package main
+// //package main
+// //
+// //import "fmt"
+// //
+// //func main() {
+// //	m := make(map[string]int, 4)
+// //	m["a"] = 1
+// //	m["b"] = 2
+// //	m["c"] = 3
+// //	m["d"] = 4
+// //	fmt.Println(m)
+// //
+// //}
 //
-//import "fmt"
+// package main
 //
-//func main() {
-//	m := make(map[string]int, 4)
-//	m["a"] = 1
-//	m["b"] = 2
-//	m["c"] = 3
-//	m["d"] = 4
-//	fmt.Println(m)
+// import "fmt"
 //
-//}
-
+//	func main() {
+//		// 新建一个容量为4的字典 map
+//		m := make(map[string]int64, 4)
+//		// 放三个键值对
+//		m["dog"] = 1
+//		m["cat"] = 2
+//		m["hen"] = 3
+//		fmt.Println(m)
+//		// 查找 hen
+//		which := "hen"
+//		v, ok := m[which]
+//		if ok {
+//			// 找到了
+//			fmt.Println("find:", which, "value:", v)
+//		} else {
+//			// 找不到
+//			fmt.Println("not find:", which)
+//		}
+//		// 查找 ccc
+//		which = "ccc"
+//		v, ok = m[which]
+//		if ok {
+//			// 找到了
+//			fmt.Println("find:", which, "value:", v)
+//		} else {
+//			// 找不到
+//			fmt.Println("not find:", which)
+//		}
+//	}
 package main
 
-import "fmt"
+import "sync"
 
-func main() {
-	// 新建一个容量为4的字典 map
-	m := make(map[string]int64, 4)
-	// 放三个键值对
-	m["dog"] = 1
-	m["cat"] = 2
-	m["hen"] = 3
-	fmt.Println(m)
-	// 查找 hen
-	which := "hen"
-	v, ok := m[which]
-	if ok {
-		// 找到了
-		fmt.Println("find:", which, "value:", v)
-	} else {
-		// 找不到
-		fmt.Println("not find:", which)
+type SET struct {
+	m    map[int]struct{}
+	len  int
+	lock sync.Mutex
+}
+
+func NEWset(cap int) *SET {
+	temp := make(map[int]struct{}, cap)
+	return &SET{
+		m: temp,
 	}
-	// 查找 ccc
-	which = "ccc"
-	v, ok = m[which]
-	if ok {
-		// 找到了
-		fmt.Println("find:", which, "value:", v)
-	} else {
-		// 找不到
-		fmt.Println("not find:", which)
+}
+
+func (s *SET) Add(elem int) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.m[elem] = struct{}{}
+	//s.len++
+	s.len = len(s.m)
+}
+
+func (s *SET) Remove(elem int) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if s.len == 0 {
+		return
 	}
+	delete(s.m, elem)
+	s.len--
+}
+
+func (s *SET) has(item int) bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	_, ok := s.m[item]
+	return ok
 }
