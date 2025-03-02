00a16ed2d8fa3de0011183302037b4592e5f2341
wqh <1914792977@qq.com>
2025-03-02 09:59:20 +0800

    1

--- hello/sjjg/pxsf.go
+++ hello/sjjg/pxsf.go
@@ -27,6 +27,25 @@ func bubble(ac []int) {
 	}
 }
 
+func selection(ac []int) {
+	n := len(ac)
+	//var q =0//交换中间值
+	var w = 0 //交换的序数
+
+	for i := 0; i < n; i++ {
+		var min = ac[0] //当前最小值
+		for j := i; j < n; j++ {
+			if ac[j] < min {
+				min = ac[j]
+				w = j
+			}
+		}
+		//q = ac[i]
+		ac[w] = ac[i]
+		ac[i] = min
+	}
+}
+
 func main() {
 
 }
