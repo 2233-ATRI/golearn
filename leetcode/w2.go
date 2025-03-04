a3887e54e9689c1e65fc525b905f5d9e66c8c658
wqh <1914792977@qq.com>
2025-03-04 23:30:00 +0800

    1

---
+++ leetcode/w2.go
@@ -0,0 +1,28 @@
+package main
+
+//给你一个正整数数组 nums ，对 nums 所有元素求积之后，找出并返回乘积中 不同质因数 的数目。
+//
+//注意：
+//
+//质数 是指大于 1 且仅能被 1 及自身整除的数字。
+//如果 val2 / val1 是一个整数，则整数 val1 是另一个整数 val2 的一个因数。
+//func distinctPrimeFactors(nums []int) int {
+//	var s int =1
+//	//for p:=0; p<len(nums);p++ {
+//	//	s =s*nums[p]//得到元素积
+//	//}
+//	var i =2
+//	for {
+//		if i ==
+//	}
+//
+//} //函数进行循环，当不可以被2整除的时候递归到3，然后判断除数是否可以被比其小的数不可整除即可
+
+func distinctPrimeFactors(nums []int) int {
+
+}
+
+// 直接计算每个元素的质数解，每次除法之前先
+func main() {
+
+}
